0239fba02f9a1cfeb0f7b66f1a70c2e2a49c0502
Richard Hernandez <richardhz@users.noreply.github.com>
2022-04-12 16:47:22 +0100

    Use font awesome change DocumentList cs file testing spinner in About page

---
+++ RhzHome01/Client/Pages/DocumentList.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using RhzHome01.Client.Services.Interfaces;
+using RhzHome01.Shared;
+
+namespace RhzHome01.Client.Pages;
+
+public partial class DocumentList
+{
+
+    [Inject]
+    NavigationManager NavManager { get; set; }
+    [Inject]
+    protected IRhzViewData ViewDataService { get; set; }
+    [Inject] ICacheService CacheService { get; set; }
+    protected IEnumerable<PostContentDto> Documents { get; set; }
+    protected IEnumerable<LinkContentDto> InterestingLinks { get; set; }
+    protected IEnumerable<LinkContentDto> DotNetLinks { get; set; }
+    private DocumentListData Data { get; set; }
+    protected override async Task OnInitializedAsync()
+    {
+        Data = CacheService.Get<DocumentListData>("Documents");
+        if (Data == null)
+        {
+            Data = await ViewDataService.GetDocumentsViewModel().ConfigureAwait(false);
+            CacheService.Add("Documents", Data, Data.MaxAge);
+        }
+
+        Documents = Data.Documents;
+        InterestingLinks = Data.InterestingLinks;
+        DotNetLinks = Data.DotNetLinks;
+    }
+
+    protected void GotoPage(string name)
+    {
+        NavManager.NavigateTo($"/documents/{name}");
+    }
+}
